19f3126e1a473e852bc9c62096a6925176dc6312
han-nwin <hannguyen.win@gmail.com>
2024-10-07 13:25:35 -0500

    ok

---
+++ 2fork.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid, pid2;
+
+    // First set of fork operations
+    if (pid = fork() && pid2 = fork()) {
+        fork();
+    }
+
+    // Second set of fork operations
+    if (pid = fork() && pid2 = fork()) {
+        fork();
+    }
+
+    // Third set of fork operations
+//    if ((pid = fork()) && (pid2 = fork())) {
+  //      fork();
+    //}
+
+    // Print the PID of each process created
+    printf("Process ID: %d, Parent ID: %d\n", getpid(), getppid());
+
+    return 0;
+}
+
